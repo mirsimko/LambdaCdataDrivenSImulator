f99efcc73def115c2cb707404002ac406944677d
Miroslav Simko <mirsimko@gmail.com>
2016-01-28 02:19:37 -0500

    Corrected centrality for 2D histos

--- create2Dhistos.C
+++ create2Dhistos.C
@@ -18,48 +18,45 @@
 #include <iostream>
 using namespace std;
 
-TH3D* getHisto(TFile &fDca1, int iEta, int iVz, int iCent) ;
-
-// ----------------------------------------------------------------------------
 void create2Dhistos()
 {
   // contstants
-  // const Int_t nParticles = 3;
-  const int nVzs = 4;
-  const int nEtas = 5;
-  const int nCentDca = 6;
+  const Int_t nParticles = 3;
+  int const nVzs = 4;
+  int const nEtas = 5;
+  int const nCentDca = 8;
   const Int_t nPtBins = 24;
-  const double epsilon = 0.0001;
+  const double epsilon = 0.000001;
 
   // input file and output file
   TFile fDca1("IncludeProton/DCA/Proton/NoBinWidth_3D_Dca_VsPt_Centrality_Eta_Phi_Vz_Zdcx_proton.root");
-  TFile *outHistF = new TFile("IncludeProton/DCA/Proton/2DProjection_simCent_NoBinWidth_3D_Dca_VsPt_Centrality_Eta_Phi_Vz_Zdcx_proton_trimmed.root", "RECREATE");
-
-  char n;
-  cout << "Press any key" << endl;
-  cin >> n;
-
-  // edges of the pt bins in 3D DCA histograms
+  TFile *outHistF = new TFile("IncludeProton/DCA/Proton/2DProjection_simCent_NoBinWidth_3D_Dca_VsPt_Centrality_Eta_Phi_Vz_Zdcx_proton.root", "RECREATE");
+    
+  // char n;
+  // cout << "Press any key" << endl;
+  // cin >> n;
+  // edbes of the pt bins in 3D DCA histograms
   const Double_t ptEdge[nPtBins + 1] =
-  { 0. , 0.2 , 0.3 , 0.4 , 0.5 , 0.6 , 0.7 , 0.8 , 0.9 ,
+ {
+    0. , 0.2 , 0.3 , 0.4 , 0.5 , 0.6 , 0.7 , 0.8 , 0.9 ,
     1. , 1.2 , 1.4 , 1.6 , 1.8 , 2.  , 2.2 , 2.4 , 2.6 , 2.8 ,
     3. , 3.5, 4.  , 4.5 , 5., 12.0};
 
-  // edges of the centrality in 3D DCA histograms
-  const Int_t centEdge[nCentDca + 1] = 
-  { 0, 1, 2, 3, 4, 6, 8};
-
-  for (int iEta = 0; iEta < nEtas; ++iEta)
+  for(int iCent = 0; iCent < nCentDca; ++iCent)
   {
-    for (int iVz = 0; iVz < nVzs; ++iVz)
+    for (int iEta = 0; iEta < nEtas; ++iEta)
     {
-      // index of centrality in 3D histograms
-      int iCent3d = 0;
-
-      for(int iCent = 0; iCent < nCentDca; ++iCent)
+      for (int iVz = 0; iVz < nVzs; ++iVz)
       {
-	// get the 3D histo
-	TH3D *hist3D = getHisto(fDca1, iEta, iVz, iCent3d);
+	// Getting 3D histogram
+	TH3D* hist3D = 0;
+	const char *h3dName = Form("mh3DcaXyZPtCentPartEtaVz_%i_%i_%i_%i", 0, iEta, iVz, iCent+2); 
+	hist3D = (TH3D*)(fDca1.Get(h3dName));
+	if (!hist3D)
+	{
+	  cerr << "histogram \"" << h3dName << "\" not found." << endl;
+	  throw;
+	}
 
 	// getting parameters of the 3D histogram (nBins, min and max of the axes, and title)
 	int nXyBins = hist3D->GetYaxis()->GetNbins();
@@ -86,72 +83,35 @@ void create2Dhistos()
 	  hist2D.GetXaxis()->SetTitle(xyTitle);
 	  hist2D.GetYaxis()->SetTitle(zTitle);
 
-	  while ( iCent3d  < centEdge[iCent+1] )
+	  while ( (hist3D->GetXaxis()->GetBinUpEdge(iPt3d)) - epsilon < ptEdge[iPt+1])
 	  {
-	    // get 3D histo
-	    hist3D = getHisto(fDca1, iEta, iVz, iCent3d);
-	    cout << "Title:" << h2dTitle << ", centrality: " << iCent << ", pT: " << iPt << endl;
-
-	    while ( (hist3D->GetXaxis()->GetBinUpEdge(iPt3d)) - epsilon < ptEdge[iPt+1])
+	    // filling the 2D histogram
+	    for (int iXy = 0; iXy < nXyBins+2; ++iXy)	  
 	    {
-	      // filling the 2D histogram
-	      for (int iXy = 0; iXy < nXyBins+2; ++iXy)
+	      for (int iZ = 0; iZ < nZBins+2; ++iZ)
 	      {
-		for (int iZ = 0; iZ < nZBins+2; ++iZ)
-		{
-		  // copy bin content
-		  double last =  hist2D.GetBinContent(iXy, iZ) ;
-		  hist2D.SetBinContent(iXy, iZ, hist3D->GetBinContent(iPt3d,iXy,iZ) + last );
-		}
+		// copy bin content
+		double last =  hist2D.GetBinContent(iXy, iZ) ;
+		hist2D.SetBinContent(iXy, iZ, hist3D->GetBinContent(iPt3d,iXy,iZ) + last );
 	      }
-	      // raise index of pT bin of 3D hist
-	      ++iPt3d;
 	    }
-	    // raise index of Centrality 3D hist
-	    ++iCent3d;
-	    // delete the 3D histo
-	    // delete hist3D;
+	    // raise index of pT bin
+	    ++iPt3d;
 	  }
 
 	  // testing if some particles were recorded
-	  // if (hist2D.Integral() == 0)
-	  // {
-	  //   // this means something went wrong
-	  //   cerr << "Number of particles recorded: " << hist2D.Integral() 
-	  //     << " from pT histogram: " << iPt 
-	  //     << " between " << ptEdge[iPt] << " and " << ptEdge[iPt+1] << endl;
-	  //   cerr << "Centrality of 2D: " << iCent << ", Centrality of 3D: " << iCent3d << endl;
-	  //   cerr << ", pT bin number: " << iPt3d 
-	  //     << " between " << hist3D->GetXaxis()->GetBinLowEdge(iPt3d-1)  << " and " << hist3D->GetXaxis()->GetBinUpEdge(iPt3d-1) << endl;
-	  //   cerr << "Title: " << h2dTitle << endl;
-	  //   cerr << "Name: " << h2dName << endl;
-	  //   throw;
-	  // }
-
+	  if (hist2D.Integral() == 0)
+	    cout << "Number of particles recorded: " << hist2D.Integral() 
+	      << " from pT histogram: " << iPt << " between " << ptEdge[iPt] << " and " << ptEdge[iPt+1]
+	      << ", pT bin number: " << iPt3d << " between " << hist3D->GetXaxis()->GetBinLowEdge(iPt3d-1)  << " and " << hist3D->GetXaxis()->GetBinUpEdge(iPt3d-1) << endl;
 	  // Saving the 2D histogram
 	  outHistF->cd();
-	  hist2D.Write();  
+	  hist2D.Write();	  
 	}
       }
     }
-    cout<<"Finished writing histograms for Eta " << iEta << endl;
+    cout<<"Finished writing histograms for centrality " << iCent << endl;
   }
   cout << endl;
   cout << "Done..." << endl;
 }
-
-// ----------------------------------------------------------------------------
-// Getting 3D histogram
-TH3D* getHisto(TFile &fDca1, int iEta, int iVz, int iCent) 
-{
-  TH3D* hist3D = 0;
-  const char *h3dName = Form("mh3DcaXyZPtCentPartEtaVz_%i_%i_%i_%i", 0, iEta, iVz, iCent+2); 
-  hist3D = (TH3D*)(fDca1.Get(h3dName));
-  if (!hist3D)
-  {
-    cerr << "getHisto: histogram \"" << h3dName << "\" not found." << endl;
-    throw;
-  }
-  return hist3D;
-}
-// ----------------------------------------------------------------------------
